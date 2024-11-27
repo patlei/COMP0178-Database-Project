3fd094fb5f1b18261cc98cbf6f7532f84da10342
racheiii <rachelseahyt@gmail.com>
2024-11-27 02:11:53 +0000

    Footer completed

--- base code/footer.php
+++ base code/footer.php
@@ -1,17 +1,63 @@
-</html>
-
 <!-- Footer -->
-<!-- Bootstrap core JavaScript -->
+<footer class="bg-light text-dark pt-4">
+  <div class="container">
+    <div class="row">
+      <!-- About Section -->
+      <div class="col-md-3">
+        <h5>About Us</h5>
+        <p>Discover the charm of knitting and crochet with our auction platform, where creativity meets community. </p>
+      </div>
+
+      <!-- Quick Links -->
+      <div class="col-md-3">
+        <h5>Quick Links</h5>
+        <ul class="list-unstyled">
+          <li><a href="browse.php" class="text-dark">Browse Auctions</a></li>
+          <li><a href="create_auction.php" class="text-dark">Create an Auction</a></li>
+          <li><a href="profile.php" class="text-dark">My Profile</a></li>
+        </ul>
+      </div>
+
+      <!-- Support -->
+      <div class="col-md-3">
+        <h5>Support</h5>
+        <ul class="list-unstyled">
+          <li><a class="text-dark">Contact Us</a></li>
+          <li><a class="text-dark">Report an Issue</a></li>
+          <li><a class="text-dark">Feedback</a></li>
+        </ul>
+      </div>
+
+      <!-- Follow Us -->
+      <div class="col-md-3">
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://instagram.com" class="text-dark mr-3"><i class="fa fa-instagram"></i></a>
+          <a href="https://facebook.com" class="text-dark mr-3"><i class="fa fa-facebook"></i></a>
+          <a href="https://pinterest.com" class="text-dark"><i class="fa fa-pinterest"></i></a>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col text-center">
+        <p>&copy; 2024 Knitty Gritty Online Store. All rights reserved.</p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<!-- JavaScript and Bootstrap Scripts -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script>
 
-<!-- Ensure dropdown functionality -->
+<!-- Dropdown Initialisation -->
 <script>
-    $(document).ready(function () {
-        console.log("Dropdowns initialized.");
-        $('.dropdown-toggle').dropdown(); // Ensure dropdown initialization
-    });
+  $(document).ready(function () {
+      console.log("Dropdowns initialized.");
+      $('.dropdown-toggle').dropdown(); // Ensure dropdown initialization
+  });
 </script>
 
 </body>
